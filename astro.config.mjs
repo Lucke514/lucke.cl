5ab6842dd470d5e415c352429c04bce5bd659466
Lucke514 <lucas.gonzalez.espinosa@outlook.com>
2024-11-21 22:32:05 -0300

    Remove Astro Image integration from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [image()]
 });
