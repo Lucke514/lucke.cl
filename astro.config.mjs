96f85c2db7b55f254d184fdd192f2729620a1031
Lucke514 <lucas.gonzalez.espinosa@outlook.com>
2025-06-19 20:27:00 -0400

    Update astro.config.mjs to remove server configuration; add live link for Organizer project in proyectos.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  server : {
-    port : 3000,
-    host : true
-  }
+  output: 'static',
+  integrations: [tailwind()]
 });
